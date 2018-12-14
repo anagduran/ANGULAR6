a627b3ca9bfd793b4a135cfb2d360af06726f2dd
unknown <agabrieladuran1@gmail.com>
2018-12-14 11:22:24 -0400

    agregando component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationInfoComponent } from './application-info/application-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/application-info/application-info.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicacion-info',
+  templateUrl: './application-info.component.html',
+  styleUrls: ['./application-info.component.css']
+})
+export class ApplicationInfoComponent {
+  info: string;
+}
+
